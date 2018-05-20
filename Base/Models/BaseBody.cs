83a51bedeba86cebb9b9f18ca34cf151d483b01c
mgmtacccfs <mgmt.acc.cfs@gmail.com>
2018-05-20 07:04:29 -0300

    Update 3

--- Base/Models/BaseBody.cs
+++ Base/Models/BaseBody.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Media;
 
 namespace Base.Models
 {
     public class BaseBody : BaseGrid
     {
+        private ColumnDefinition clnDefinition = null;
+
         public BaseBody() : base("BaseBodyGrid", BaseGrid.Theme.Light, true)
         {
+            this.loadLayout();
+            this.loadControls();
+        }
 
+        private void addColumn(double dblWidth)
+        {
+            this.clnDefinition = new ColumnDefinition();
+            this.clnDefinition.Width = new GridLength(dblWidth, GridUnitType.Star);
+            this.ColumnDefinitions.Add(this.clnDefinition);
+        }
+
+        private void loadLayout()
+        {
+            this.addColumn(0.24);
+            this.addColumn(19.52);
+            this.addColumn(0.24);
+        }
+
+        private void loadControls()
+        {
+            
         }
     }
 }
